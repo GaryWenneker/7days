e8242cf9e47b02f8c09742cdbf67e939e7106a8b
Gary Wenneker <gary@gary.nu>
2023-02-02 11:31:44 +0100

    bing and google search

--- src/blocks/SearchBox.js
+++ src/blocks/SearchBox.js
@@ -10,8 +10,12 @@ export default function Searchbox({ data }) {
     return win.open(url, windowName, `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${w}, height=${h}, top=${y}, left=${x}`);
   }
 
-  function search (value) {
-    popupWindow(`https://www.google.nl/search?q=site%3Akidsweek.nl+${value}`, 'google', window, 1240, 800);
+  function searchGoogle (value) {
+    popupWindow(`https://www.google.nl/search?q=site%3A7dagen.netlify.app+${value}`, 'google', window, 1240, 800);
+  }
+
+  function searchBing (value) {
+    popupWindow(`https://www.bing.com/search?q=site%3A7dagen.netlify.app+${value}`, 'google', window, 1240, 800);
   }
   
 
@@ -21,18 +25,46 @@ export default function Searchbox({ data }) {
         <label htmlFor='default-search' className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
         <div className="relative">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                <svg aria-hidden="true" 
+                className="w-5 h-5 text-gray-500 dark:text-gray-400" 
+                fill="none" 
+                stroke="currentColor" 
+                viewBox="0 0 24 24" 
+                xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             </div>
             <input type="search" 
-            id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Zoeken binnen deze site..." required
+            id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Zoeken met Google binnen deze site..." required
             
             onKeyPress={event => {
                 if (event.key === 'Enter') {
-                  search(event.target.value)
+                  searchGoogle(event.target.value)
                 }
               }}
             />
-            <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Zoeken</button>
+        </div>
+      </Container>
+      <Container className='py-5'>
+        <label htmlFor='default-search' className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+        <div className="relative">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg aria-hidden="true" 
+                className="w-5 h-5 text-gray-500 dark:text-gray-400" 
+                fill="none" 
+                stroke="currentColor" 
+                viewBox="0 0 24 24" 
+                xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+            </div>
+            <input type="search" 
+            id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Zoeken met Bing binnen deze site..." required
+            
+            onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  searchBing(event.target.value)
+                }
+              }}
+            />
         </div>
       </Container>
     </section>    
